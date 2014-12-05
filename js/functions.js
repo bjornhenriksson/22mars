2b077d0757704c42f7825da7e6c5f1d4151c5255
Björn Henriksson <bjarna2@gmail.com>
2014-12-05 11:27:20 +0100

    removed content in functions.js

--- js/functions.js
+++ js/functions.js
@@ -1,21 +1,5 @@
 jQuery(document).ready(function ($) {
 
-    $(".alert").delay(3000).fadeOut();
-
-    $(".details-form").hide();
-
-    $(".enter-form").submit(function( event ) {
-    	event.preventDefault();
-    	$(".fear-form").addClass("slide");
-    	$(".login-form").addClass("slide-right");
-    	//this.submit();
-    });
-
-    $(".show-form").click(function() {
-    	$(".button-fb, .show-form").fadeOut();
-    	var holder = $("#holder").val();
-    	$(".apply-holder").val(holder);
-    	$(".details-form").delay(400).slideDown();
-	});
+    
 
 });   
